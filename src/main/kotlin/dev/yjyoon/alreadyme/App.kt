d511ea01940f290ddec67917b5121d1dff62eab2
yjyoon-dev <yjyoon.dev@gmail.com>
2022-07-29 17:47:57 +0900

    fix: Main 으로부터 App 분리

---
+++ src/main/kotlin/dev/yjyoon/alreadyme/App.kt
@@ -0,0 +1,25 @@
+package dev.yjyoon.alreadyme
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+@Preview
+fun App() {
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    MaterialTheme {
+        Button(onClick = {
+            text = "Hello, Desktop!"
+        }) {
+            Text(text)
+        }
+    }
+}
