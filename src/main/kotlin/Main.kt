87e09b644d1810cae45356ae077df9edd25d563d
yjyoon-dev <yjyoon.dev@gmail.com>
2022-08-03 18:07:36 +0900

    feat: control panel 커스텀 및 윈도우 사이즈 설정

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,25 @@
+import androidx.compose.foundation.window.WindowDraggableArea
+import androidx.compose.material.TopAppBar
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import dev.yjyoon.alreadyme.App
+import dev.yjyoon.alreadyme.ui.value.R
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
-        title = "ALREADYME.md"
+        title = R.string.APP_TITLE,
+        undecorated = true,
+        state = WindowState(position = WindowPosition(Alignment.Center), width = 1280.dp, height = 800.dp)
     ) {
+        WindowDraggableArea {
+            TopAppBar(backgroundColor = R.color.DarkGray) {
+            }
+        }
         App()
     }
 }
