463d3f78974b85e1fb94e65adbcff4da1c7ca98a
yjyoon-dev <yjyoon.dev@gmail.com>
2022-09-09 19:24:12 +0900

    fix: FailureScreen 일부 UI 수정

--- src/main/kotlin/dev/yjyoon/alreadyme/ui/feature/failure/FailureScreen.kt
+++ src/main/kotlin/dev/yjyoon/alreadyme/ui/feature/failure/FailureScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -61,13 +62,14 @@ fun FailureScreen(
                         fontSize = 92.sp,
                         color = MaterialTheme.colors.surface
                     )
+                    Spacer(modifier = Modifier.height(4.dp))
                     Text(
                         text = R.string.ERROR_MESSAGE,
                         fontSize = 28.sp,
+                        fontWeight = FontWeight.Light,
                         color = MaterialTheme.colors.surface
                     )
                 }
-
             }
         }
         Box(
