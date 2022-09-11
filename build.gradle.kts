8cf17c20becc09b4c96963a15dfa97296df87fdf
yjyoon-dev <yjyoon.dev@gmail.com>
2022-09-12 00:57:45 +0900

    feat :build 확장자 msi -> exe 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,7 +49,7 @@ compose.desktop {
     application {
         mainClass = "MainKt"
         nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            targetFormats(TargetFormat.Dmg, TargetFormat.Exe, TargetFormat.Deb)
             packageName = "alreadyme"
             packageVersion = "1.0.0"
         }
