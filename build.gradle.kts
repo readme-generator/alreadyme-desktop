5c7a83759ade57ef55be2735327969aa6dfc93ce
yjyoon-dev <yjyoon.dev@gmail.com>
2022-10-10 22:07:36 +0900

    update dependeices versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     val kotlinVersion = "1.6.10"
     kotlin("jvm") version kotlinVersion
     kotlin("kapt") version kotlinVersion
-    id("org.jetbrains.compose") version "1.1.0"
+    id("org.jetbrains.compose") version "1.1.1"
     id("org.jetbrains.kotlin.plugin.serialization") version kotlinVersion
 }
 
@@ -22,23 +22,24 @@ dependencies {
     implementation(compose.desktop.currentOs)
 
     // Ktor client
-    val ktorVersion = "2.0.3"
+    val ktorVersion = "2.1.2"
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-logging-jvm:$ktorVersion")
     implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
 
     // Dagger 2
-    val daggerVersion = "2.43.1"
+    val daggerVersion = "2.44"
     implementation("com.google.dagger:dagger:$daggerVersion")
     kapt("com.google.dagger:dagger-compiler:$daggerVersion")
 
     // Logging
-    implementation("ch.qos.logback:logback-classic:1.2.11")
+    implementation("ch.qos.logback:logback-classic:1.4.3")
 
     implementation("com.mikepenz:multiplatform-markdown-renderer:0.6.1")
 
     implementation("javax.inject:javax.inject:1")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.withType<KotlinCompile>() {
@@ -55,4 +56,12 @@ compose.desktop {
             packageVersion = "1.0.0"
         }
     }
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
