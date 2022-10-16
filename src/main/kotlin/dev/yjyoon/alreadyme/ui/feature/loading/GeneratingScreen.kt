2358fd9a95fe9771b5f381a8790108f1fc8d6017
yjyoon-dev <yjyoon.dev@gmail.com>
2022-10-16 20:30:14 +0900

    feat: auto-scrolling on receive generating readme

--- src/main/kotlin/dev/yjyoon/alreadyme/ui/feature/loading/GeneratingScreen.kt
+++ src/main/kotlin/dev/yjyoon/alreadyme/ui/feature/loading/GeneratingScreen.kt
@@ -2,12 +2,15 @@ package dev.yjyoon.alreadyme.ui.feature.loading
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -50,20 +53,30 @@ fun GeneratingScreen(
                 shape = RoundedCornerShape(8.dp),
                 border = BorderStroke(width = 2.dp, color = R.color.DarkGray.copy(alpha = 0.12f))
             ) {
-                Markdown(
-                    content = generatingReadme,
-                    modifier = Modifier.padding(24.dp),
-                    typography = MarkdownDefaults.markdownTypography(
-                        h1 = MarkdownTypography.h1,
-                        h2 = MarkdownTypography.h2,
-                        h3 = MarkdownTypography.h3,
-                        h4 = MarkdownTypography.h4,
-                        h5 = MarkdownTypography.h5,
-                        h6 = MarkdownTypography.h6,
-                        body1 = MarkdownTypography.body1,
-                        body2 = MarkdownTypography.body2
+                val scrollState = rememberScrollState()
+
+                Column(
+                    modifier = Modifier.verticalScroll(scrollState)
+                ) {
+                    Markdown(
+                        content = generatingReadme,
+                        modifier = Modifier.padding(24.dp),
+                        typography = MarkdownDefaults.markdownTypography(
+                            h1 = MarkdownTypography.h1,
+                            h2 = MarkdownTypography.h2,
+                            h3 = MarkdownTypography.h3,
+                            h4 = MarkdownTypography.h4,
+                            h5 = MarkdownTypography.h5,
+                            h6 = MarkdownTypography.h6,
+                            body1 = MarkdownTypography.body1,
+                            body2 = MarkdownTypography.body2
+                        )
                     )
-                )
+                }
+
+                LaunchedEffect(generatingReadme) {
+                    scrollState.animateScrollTo(scrollState.maxValue)
+                }
             }
             Column(
                 modifier = Modifier
